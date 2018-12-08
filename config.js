2a375ba03af750f1e007be88b4afac1301ae20bb
Manvel Saroyan <saroyanm@gmail.com>
2018-12-08 18:48:18 +0100

    Updated styles according to the changes in the design

---
+++ config.js
@@ -0,0 +1,21 @@
+const templateData = {
+  "social": [
+    {
+      name: "Github",
+      image: "images/github.svg",
+      path: "https://github.com/Manvel"
+    },
+    {
+      name: "Linkedin",
+      image: "images/linkedin.svg",
+      path: "https://www.linkedin.com/in/saroyanm/"
+    },
+    {
+      name: "Twitter",
+      image: "images/twitter.svg",
+      path: "https://twitter.com/saroyanm"
+    }
+  ]
+};
+
+module.exports = {templateData};
--- pages/blog-post-2.md
+++ pages/blog-post-2.md
@@ -1,7 +1,7 @@
 ---
 title: blog post 2
 description: Description of the blog post 2
-summary: Summary for the blog post 2
+summary: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac ultrices ipsum. Curabitur vehicula vehicula mi, non ultrices orci commodo eu. Nunc blandit orci condimentum erat tempus, at ultrices ex sodales.
 header: CMintS starter
 type: post
 ---
